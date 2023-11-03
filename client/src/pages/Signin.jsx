e55059feebb741099b6d792ed6988c4d711e2eb6
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-11-03 15:31:04 +0530

    signin done

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
+
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json(); // Parse response as JSON
+    console.log(data);
+  }
+
   return (
-    <div>
+    <>
       <Header/>
-      Signin
-    </div>
+      <div className='p-5 max-w-lg mx-auto'>
+        <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          {/* <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} /> */}
+          <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+          <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Sign In</button>
+        </form>
+        <div className='flex gap-2 mt-5'>
+          <p> Dont have an account</p>
+          <Link to={"/signup"}>
+            <span className='text-blue-700'>Sign up</span>
+          </Link> 
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Signin
+export default Signin;
